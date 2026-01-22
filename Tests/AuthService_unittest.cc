ff79184c920c56a9b3d9daf0e69751efde180d94
IOSA33 <sedinkin33@gmail.com>
2026-01-22 20:28:39 +0200

    added tests

--- Tests/AuthService_unittest.cc
+++ Tests/AuthService_unittest.cc
@@ -4,7 +4,35 @@
 namespace authTest {
     TEST(parserTest, TooManyArgs) {
         AuthService s{};
-        EXPECT_EQ(false, s.parser("test@gmail.com 6543 err"));
+
+        EXPECT_FALSE(s.parser("test@gmail.com 6543 err"));
+        EXPECT_FALSE(s.parser("test@gmail.com"));
+        EXPECT_FALSE(s.parser("test@gmail.com 6543"));
+        EXPECT_FALSE(s.parser(""));
+        EXPECT_FALSE(s.parser("REG test@gmail.com"));
+        EXPECT_FALSE(s.parser("REG"));
+        EXPECT_FALSE(s.parser("REG test@gmail.com 4321 arg"));
+        EXPECT_FALSE(s.parser("LOG test@gmail.com"));
+        EXPECT_FALSE(s.parser("LOG test@gmail.com 1234"));
+        EXPECT_FALSE(s.parser("LOG"));
+        EXPECT_FALSE(s.parser("LOG test@gmail.com 4321 arg"));
+        EXPECT_FALSE(s.parser("GET test@gmail.com 4321"));
+        EXPECT_FALSE(s.parser("GET"));
+    }
+
+    TEST(parserTest, CorrectArgs) {
+        {
+            AuthService s{};
+            EXPECT_TRUE(s.parser("REG test@gmail.com 6543"));
+        }
+        {
+            AuthService s{};
+            EXPECT_TRUE(s.parser("LOG test@gmail.com 6543"));
+        }
+        {
+            AuthService s{};
+            EXPECT_TRUE(s.parser("GET test"));
+        }
     }
 }
 
